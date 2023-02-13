0e55ea3cb295ae0d8472505763ac89545ca2f3dc
Erin Friedman <erin.friedman@ymail.com>
2023-02-13 16:09:14 -0600

    Added function to find average character length of names in a names array

--- arrarysLoopsAndFunctions.js.js
+++ arrarysLoopsAndFunctions.js.js
@@ -13,10 +13,22 @@ console.log(ages[ages.length - 1] - (ages[0])); //expected outcome 27
 function findAverage(ages){ //write function that takes ages
     let average = 0; //create variable to hold average
     for (let i = 0; i < ages.length; i++){//loop through the array
-        let currentNum = ages[i]; //add empty vairable to each index
-        average += currentNum;
+        average += ages[i]; //statment to calculate average
     }
     average = average / ages.length //divide by the length
     return average;//return variable
 }
 console.log(findAverage([3, 9, 23, 64, 2, 8, 28, 93])); //call on function 
+
+
+let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']; //names array
+
+function findNameAverage(names){ //write function with names as the parameter
+    let average = 0 //create variable to hold average
+    for (let x = 0; x < names.length; x++){ //loop through array
+    average += names[x].length; //statemnt to calculate average characters
+    }
+    average = average / names.length // divide by length
+    return average; //return variable
+}
+console.log(findNameAverage(['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'])); // call on function, expected outcome 3.83
