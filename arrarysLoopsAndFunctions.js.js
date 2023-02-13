bb97022f08954ffa800efa4cc80062b8dc220b6e
Erin Friedman <erin.friedman@ymail.com>
2023-02-13 15:48:44 -0600

    adding a function with a for loop to calculate average age

--- arrarysLoopsAndFunctions.js.js
+++ arrarysLoopsAndFunctions.js.js
@@ -6,5 +6,17 @@ ages[0];                // locating the first index element
 
 console.log((ages[ages.length -1]) - (ages[0])); //printing result of last index element minus first index element
 
-ages.push(30);
-console.log(ages[ages.length - 1] - (ages[0]));
+ages.push(30); //adding a new element to the array
+console.log(ages[ages.length - 1] - (ages[0])); //expected outcome 27
+
+
+function findAverage(ages){ //write function that takes ages
+    let average = 0; //create variable to hold average
+    for (let i = 0; i < ages.length; i++){//loop through the array
+        let currentNum = ages[i]; //add empty vairable to each index
+        average += currentNum;
+    }
+    average = average / ages.length //divide by the length
+    return average;//return variable
+}
+console.log(findAverage([3, 9, 23, 64, 2, 8, 28, 93])); //call on function 
