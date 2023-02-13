3224bd29de01654e77c3795364f95a078042c9ab
Erin Friedman <erin.friedman@ymail.com>
2023-02-13 15:20:53 -0600

    1st Array - Subtracting index values

---
+++ arrarysLoopsAndFunctions.js.js
@@ -0,0 +1,7 @@
+//create an array named ages containing values 3,9,23,64,2,8,28,93
+let ages = [3, 9, 23, 64, 2, 8, 28, 93]; //ages array
+
+ages[ages.length - 1]; //locating the last index element in the array
+ages[0];                // locating the first index element
+
+console.log((ages[ages.length -1]) - (ages[0])); //printing result of last index element minus first index element
